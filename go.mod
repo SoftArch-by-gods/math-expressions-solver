1f9c44cff26d05d08656ac64a043e23fd405967a
Danil Bezus <danil.bezus.db@gmail.com>
2022-05-25 15:29:23 +0300

    Create calculating algorithm. Some package fixes

--- go.mod
+++ go.mod
@@ -1,14 +1,19 @@
-// TODO: Rename to your own module.
-module github.com/roman-mazur/architecture-lab-2
+module github.com/SoftArch-by-gods/math-expressions-solver
 
 go 1.17
 
-// TODO: Change if need to use a different assertions library.
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
+)
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/yuin/goldmark v1.4.1 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- implementation.go
+++ implementation.go
@@ -1,9 +1,72 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	st "github.com/golang-collections/collections/stack"
+	"math"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func isDigit(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func strRevers(s string) []string {
+	runes := strings.Split(s, " ")
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return runes
+}
+
+func CalculatePrefix(input string) (string, error) {
+	s := st.New()
+	str := strRevers(input)
+
+	for _, elem := range str {
+		if isDigit(elem) {
+			num, _ := strconv.ParseFloat(elem, 64)
+			s.Push(num)
+		} else {
+			val1 := s.Pop()
+			val2 := s.Pop()
+			if val1 == nil || val2 == nil {
+				return "error", errors.New("expression syntax error")
+			}
+			n1 := val1.(float64)
+			n2 := val2.(float64)
+
+			switch elem {
+			case "+":
+				s.Push(n1 + n2)
+				break
+			case "-":
+				s.Push(n1 - n2)
+				break
+			case "*":
+				s.Push(n1 * n2)
+				break
+			case "/":
+				s.Push(n1 / n2)
+				break
+			case "^":
+				s.Push(math.Pow(n1, n2))
+				break
+			default:
+				return "error", errors.New("undefined symbol")
+			}
+		}
+	}
+	if s.Len() != 1 {
+		return "error", errors.New("expression syntax error")
+	}
+	res := s.Pop()
+	fl := res.(float64)
+	out := strconv.FormatFloat(fl, 'f', -1, 64)
+	return out, nil
 }
