91118414203d98107a9d3319251fa652dfd4457d
grefle <timesetday61@gmail.com>
2022-05-25 20:23:12 +0300

    Add necessary fields and Implement

--- handler.go
+++ handler.go
@@ -1,12 +1,31 @@
 package lab2
 
+import (
+	"io"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
+// Its Compute() method read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	buf, inpErr := io.ReadAll(ch.Input)
+	if inpErr != nil {
+		return inpErr
+	}
+
+	res, calcErr := CalculatePrefix(string(buf))
+	if calcErr != nil {
+		return calcErr
+	}
+
+	_, outErr := io.WriteString(ch.Output, res)
+	if outErr != nil {
+		return outErr
+	}
+
 	return nil
 }
